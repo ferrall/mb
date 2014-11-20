91ac481a6e382ab464026a272febabb785808bc2
barberfm <barberm@econ.queensu.ca>
2014-11-20 14:42:00 -0500

    ValueIteraction Error

--- QualityConstraints.h
+++ QualityConstraints.h
@@ -81,14 +81,14 @@ struct QualityConstraints : ExtremeValue	{	 //ExpostSmoothing just copied from K
 
 	//credit passing/failing
 //	theta_0 = <-4.00,1.00,-2.04,2.40>, //un observedtype 1, 2, 3, 4
-	theta_0 = .25,
+	theta_0 = .50,
 	theta_1 = .25,	//GPA (i'll make this ability)
 	theta_2 = .0730,	//grade they are in
 	theta_3 = .05,	//age
 	theta_4 = .00999,	//no credits
-	theta_5 = -.0697,	 //no work
-	theta_6 = -.136,	 //pt work
-	theta_7 = -.102,	 //ft work
+	theta_5 = .0697,	 //no work
+	theta_6 =  -.136,	 //pt work
+	theta_7 = -.22,	 //ft work
 
 	//Random HC function
 	beta_0 = .19,
